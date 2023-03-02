84314b8f7c1cf64031d89204818ce741174260f9
kirill_shishkov <shishkov.kirill99@gmail.com>
2023-03-02 14:31:57 +0300

    refactor: Вынес функцию open() за пределы startGame()

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ function startGame() {
         if (isFirstClick === true) {
             timeCounter();
         }
-        open(row, column);
+        open(row, column, cells, bombs, openedCount, isFirstClick);
     })
 
     // Обработчики изменяющие смайл на время клика по закрытой ячейке
@@ -91,98 +91,6 @@ function startGame() {
 
         rightClick(index, cell, openedCount, bombCount);
     })
-
-    // Функция открытия ячейки
-    function open(row, column) {
-        if (!isValid(row, column)) return;
-
-        const index = row * width + column;
-        let cell = cells[index];
-
-        // Если это первый клик и попалась бомба
-        if (isBomb(row, column, bombs) && isFirstClick) {
-            let allowedIndexes = [...Array(256).keys()].filter(n => !bombs.includes(n));
-            let myIndex = bombs.indexOf(index);
-
-            if (myIndex !== -1) {
-                bombs.splice(myIndex, 1);
-                let item = allowedIndexes[Math.floor(Math.random()*allowedIndexes.length)];
-                bombs.push(item);
-            }
-
-            isFirstClick = false;
-            open(row, column);
-            return;
-        }
-
-        if (cell.disabled === true) return;
-
-        // Попали на флажок?
-        if (isFlag(index)) {
-            return;
-        } else {
-            cell.disabled = true;
-        }
-
-        // Победа, если была открыта последняя ячейка без бомбы
-        openedCount--;
-        
-        if (openedCount <= bombCount && !isBomb(row, column, bombs)) {
-            smile.style.backgroundImage = 'url(../img/smile_win.png)';
-            alert('Вы победили!');
-        }
-
-        const count = getCount(row, column, bombs);
-
-        // Попали на бомбу?
-        if (isBomb(row, column, bombs)) {
-
-            // Показываем все бомбы на поле
-            for (let i = 0; i < 16; i++) {
-                for (let j = 0; j < 16; j++) {
-                    let index = j * width + i;
-
-                    if (bombs.includes(index)) {
-                        if (isFlag(index)) {
-                            cells[index].style.backgroundImage = 'url(../img/bomb_flag.png)';
-                            cells[index].disabled = true;
-                        } else {
-                            cells[index].style.backgroundImage = 'url(../img/bomb.png)';
-                            cells[index].disabled = true;
-                        }
-                    } else {
-                        cells[index].disabled = true;
-                    }
-                }
-            }
-
-            // Красным выделяем ту, на которую нажал пользователь
-            cell.style.backgroundImage = 'url(../img/bomb_red.png)';
-
-            // Меняем смайлик при проигрыше
-            smile.style.backgroundImage = 'url(../img/smile_dead.png)';
-
-            return;
-        }
-
-        if (count !== 0) {
-            cell.style.backgroundImage = showMinesCount(count);
-            isFirstClick = false;
-            return;
-        }
-
-        if (count === 0) {
-            cell.style.backgroundImage = 'url(../img/zero_cell.png)';
-            isFirstClick = false;
-
-            for (let i = -1; i <= 1; i++) {
-                for (let j = -1; j <= 1; j++) {
-                    open(row + j, column + i);
-                }
-            }
-            return;
-        }
-    }
 }
 
 // !!! Функции, которые не должны быть в startGame()
@@ -248,4 +156,98 @@ function showMinesCount(count) {
     }
 
     return res;
+}
+
+// !!! WORK HERE!
+
+// Функция открытия ячейки
+function open(row, column, cells, bombs, openedCount, isFirstClick) {
+    if (!isValid(row, column)) return;
+
+    const index = row * width + column;
+    let cell = cells[index];
+
+    // Если это первый клик и попалась бомба
+    if (isBomb(row, column, bombs) && isFirstClick) {
+        let allowedIndexes = [...Array(256).keys()].filter(n => !bombs.includes(n));
+        let myIndex = bombs.indexOf(index);
+
+        if (myIndex !== -1) {
+            bombs.splice(myIndex, 1);
+            let item = allowedIndexes[Math.floor(Math.random()*allowedIndexes.length)];
+            bombs.push(item);
+        }
+
+        isFirstClick = false;
+        open(row, column, cells, bombs, openedCount, isFirstClick);
+        return;
+    }
+
+    if (cell.disabled === true) return;
+
+    // Попали на флажок?
+    if (isFlag(index)) {
+        return;
+    } else {
+        cell.disabled = true;
+    }
+
+    // Победа, если была открыта последняя ячейка без бомбы
+    openedCount--;
+    
+    if (openedCount <= bombCount && !isBomb(row, column, bombs)) {
+        smile.style.backgroundImage = 'url(../img/smile_win.png)';
+        alert('Вы победили!');
+    }
+
+    const count = getCount(row, column, bombs);
+
+    // Попали на бомбу?
+    if (isBomb(row, column, bombs)) {
+
+        // Показываем все бомбы на поле
+        for (let i = 0; i < 16; i++) {
+            for (let j = 0; j < 16; j++) {
+                let index = j * width + i;
+
+                if (bombs.includes(index)) {
+                    if (isFlag(index)) {
+                        cells[index].style.backgroundImage = 'url(../img/bomb_flag.png)';
+                        cells[index].disabled = true;
+                    } else {
+                        cells[index].style.backgroundImage = 'url(../img/bomb.png)';
+                        cells[index].disabled = true;
+                    }
+                } else {
+                    cells[index].disabled = true;
+                }
+            }
+        }
+
+        // Красным выделяем ту, на которую нажал пользователь
+        cell.style.backgroundImage = 'url(../img/bomb_red.png)';
+
+        // Меняем смайлик при проигрыше
+        smile.style.backgroundImage = 'url(../img/smile_dead.png)';
+
+        return;
+    }
+
+    if (count !== 0) {
+        cell.style.backgroundImage = showMinesCount(count);
+        isFirstClick = false;
+        return;
+    }
+
+    if (count === 0) {
+        cell.style.backgroundImage = 'url(../img/zero_cell.png)';
+        isFirstClick = false;
+
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                open(row + j, column + i, cells, bombs, openedCount, isFirstClick);
+            }
+        }
+        return;
+    }
 }
